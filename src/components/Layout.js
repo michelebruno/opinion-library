e7d17bada078884405a5d638b676fb0c372bb325
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-12-12 22:13:04 +0100

    Nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,18 +4,33 @@ import {Helmet} from "react-helmet";
 import {Link} from "gatsby";
 import classNames from "classnames";
 
+function MenuItem({children, to}) {
+    return <li className={"first:border-white"}>
+        <Link to={to} className={"rounded-2xl border-[1px] py-2 px-4"}
+              activeClassName={"border-light text-light"}>{children}</Link>
+    </li>
+}
+
 export default function Layout({children, className, container, wrapperClassName, fixedHeader}) {
     return <div className={classNames(wrapperClassName)}>
         <Helmet>
             <body style="overscroll-behaviour-y: none" className="bg-black text-white rel"></body>
         </Helmet>
-        <nav className={classNames(fixedHeader ? 'fixed' : 'sticky', "top-0 z-50 py-8 px-10 flex w-full uppercase justify-between text-2xl")}>
+        <nav
+            className={classNames(fixedHeader ? 'fixed' : 'sticky', "top-0 z-40 py-8 px-10 flex w-full uppercase justify-between text-lg")}>
             <Link to={"/"}>
                 WW
             </Link>
             <ul className={"flex gap-8"}>
-                <li><Link activeClassName={"pb-1 border-b-2 border-b-white"} to={"/glossary"}>Glossary</Link></li>
-                <li><Link activeClassName={"pb-1 border-b-2"} to={"/glossary"}>About</Link></li>
+                <MenuItem to={"/"}>
+                    Context
+                </MenuItem>
+                <MenuItem to={"/glossary"}>
+                    Archive
+                </MenuItem>
+                <MenuItem to={"/about"}>
+                    About
+                </MenuItem>
             </ul>
         </nav>
         <div className={classNames('relative', container && 'mx-8', className)}>
