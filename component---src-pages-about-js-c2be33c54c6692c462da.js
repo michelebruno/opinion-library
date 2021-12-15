ddbaaa7d56d9d25b58ac69cc9b262aca71318f26
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-12-15 01:39:51 +0100

    Updates

--- component---src-pages-about-js-c2be33c54c6692c462da.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkphase_03=self.webpackChunkphase_03||[]).push([[682],{5518:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=n(7294),a=n(8497);function c(){return u.createElement(a.Z,{className:"bg-white"},u.createElement("h1",null,"About"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-c2be33c54c6692c462da.js.map
