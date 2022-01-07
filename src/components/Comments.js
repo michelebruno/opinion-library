4083deb6d071fc556044163c66be166775bdd188
michelebruno <43304601+michelebruno@users.noreply.github.com>
2022-01-07 10:24:40 +0100

    Updates

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,128 +1,136 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
-import classNames from "classnames";
-import Comment from "./Comment";
-import {ArchiveButton} from "./Button";
-import {mix} from "../utils/mix";
-import {sentencesHaveWord} from "../utils/sentences";
-
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import classNames from 'classnames';
+import Comment from './Comment';
+import {ArchiveButton} from './Button';
+import {mix} from '../utils/mix';
+import {sentencesHaveWord} from '../utils/sentences';
 
 function GroupCommentList({comments, chosen, secondWord, origin}) {
+  const [limit, setLimit] = useState(100);
 
-    const [limit, setLimit] = useState(100)
+  const {filteredComments, totalComments} = useMemo(() => {
+    const thisOriginComments = comments.filter(({origin: o}) => origin === o);
+    const filteredComments = thisOriginComments.filter(
+      ({sentences}) => !secondWord || sentencesHaveWord(sentences, secondWord)
+    );
 
+    return {
+      totalComments: thisOriginComments.length,
+      filteredComments,
+    };
+  }, [secondWord, origin, chosen, comments]);
 
-    const {filteredComments, totalComments} = useMemo(() => {
-        let thisOriginComments = comments
-            .filter(({origin: o}) => origin === o)
-        const filteredComments = thisOriginComments
-            .filter(({sentences}) => {
-                return !secondWord || sentencesHaveWord(sentences, secondWord)
-            })
+  useEffect(() => {
+    // console.time(chosen)
 
+    setLimit(200);
 
-        return {
-            totalComments: thisOriginComments.length,
-            filteredComments
-        }
-    }, [secondWord, origin, chosen, comments])
+    const id = setInterval(add, 800);
 
+    function add() {
+      if (limit > filteredComments.length) {
+        return clearInterval(id);
+      }
 
-    useEffect(() => {
+      setLimit(l => {
+        if (l <= filteredComments.length) return l + 200;
 
-        setLimit(200)
+        // console.timeEnd(chosen)
 
-        const id = setInterval(add, 800);
+        return l;
+      });
+    }
 
-        function add() {
+    return () => clearInterval(id);
+  }, [chosen]);
 
-            if (limit > filteredComments.length) {
-                return clearInterval(id)
-            }
+  const perc = (100 * filteredComments.length) / totalComments;
 
-            setLimit(l => {
-                return l <= filteredComments.length ? l + 200 : l
-            })
+  const round = perc > 1 ? 0 : 1;
 
-        }
+  console.log('Limit is ', limit);
 
-        return () => clearInterval(id)
+  return (
+    <div>
+      <p className="sticky top-0 bg-black z-30 text-center text-lg pt-3 pb-4">
+        {secondWord
+          ? `${perc.toFixed(round)}% of ${origin} opinions (${
+              filteredComments.length
+            } of ${totalComments})`
+          : `All ${origin} opinions (${filteredComments.length})`}
+      </p>
 
-    }, [chosen])
-
-    let perc = (100 * filteredComments.length / totalComments)
-
-    let round = perc > 1 ? 0 : 1
-
-    console.log("Limit is ", limit)
-
-
-    return <div>
-        <p className="sticky top-0 bg-black z-30 text-center text-lg pt-3 pb-4">
-            {
-                secondWord ?
-                    `${perc.toFixed(round)}% of ${origin} opinions (${filteredComments.length} of ${totalComments})` :
-                    `All ${origin} opinions (${filteredComments.length})`
-            }
-        </p>
-
-        <div key={origin} className={"grid gap-4 pb-64 "}> {filteredComments
-            .map(
-                (c, i) => i < limit && <Comment key={c.id}
-                                                highlightWords={true}
-                                                word={chosen}
-                                                secondWord={secondWord} {...c} />
-            )}
-
-        </div>
+      <div key={origin} className="grid gap-4 pb-64 ">
+        {' '}
+        {filteredComments.map(
+          (c, i) =>
+            i < limit && (
+              <Comment key={c.id} highlightWords word={chosen} secondWord={secondWord} {...c} />
+            )
+        )}
+      </div>
     </div>
+  );
 }
 
 export default function Comments({comments, chosen, secondWord, onChangeSecondWord, distribution}) {
+  const scroller = useRef();
 
-    const scroller = useRef()
+  useEffect(() => {
+    scroller.current?.scrollTo(0, 0);
+  }, [chosen, secondWord]);
 
-    useEffect(() => {
-        scroller.current?.scrollTo(0, 0)
-    }, [chosen, secondWord])
+  return (
+    <div className="flex h-full px-8 flex-wrap ">
+      <p className="mb-4 w-full text-lg">
+        Here you can read the reasons to sign the 100 most popular promask and nomask petition
+      </p>
+      <div className="flex flex-col h-full w-full">
+        <div className="w-full sticky top-0 bg-black z-20">
+          <h2 className="text-lg text-light my-4">Filter opinions by:</h2>
+          <div className="flex flex-wrap">
+            {distribution.map(({secondWord: word, nomaskDelta}) => {
+              const isSelected = secondWord === word;
+              const isCurrent = chosen === word;
 
-
-    return <div className={"flex h-full px-8 flex-wrap "}>
-        <p className="mb-4 w-full text-lg">Here you can read the reasons to sign the 100 most popular promask and nomask
-            petition</p>
-        <div className="flex flex-col h-full w-full">
-
-            <div className={"w-full sticky top-0 bg-black z-20"}>
-                <h2 className={"text-lg text-light my-4"}>Filter opinions by:</h2>
-                <div className="flex flex-wrap">
-                    {distribution.map(({secondWord: word, nomaskDelta}) => {
-                        const isSelected = secondWord === word
-                        const isCurrent = chosen === word
-
-                        return <ArchiveButton
-                            key={word}
-                            checkbox
-                            isCurrent={isCurrent}
-                            className={"mb-2 mr-2"}
-                            isSelected={isSelected}
-                            onClick={() => onChangeSecondWord(isSelected ? undefined : word)}
-                            style={{
-                                '--gradient-mix': mix('EA3C9A', '3514FF', nomaskDelta * 100)
-                            }}
-                        >
-                            {word}
-                        </ArchiveButton>
-                    })}
-                </div>
-            </div>
-            <div
-                className={classNames("transition-transform w-full h-full md:w-11/12 3xl:w-10/12 mx-auto overflow-y-scroll")}
-                id={"comments-container"} ref={scroller}>
-                <div className="grid grid-cols-2 gap-8 justify-around ">
-                    {['promask', 'nomask'].map(origin => <GroupCommentList key={origin} origin={origin}
-                                                                           comments={comments}
-                                                                           chosen={chosen} secondWord={secondWord}/>)}
-                </div>
-            </div>
+              return (
+                <ArchiveButton
+                  key={word}
+                  checkbox
+                  isCurrent={isCurrent}
+                  className="mb-2 mr-2"
+                  isSelected={isSelected}
+                  onClick={() => onChangeSecondWord(isSelected ? undefined : word)}
+                  style={{
+                    '--gradient-mix': mix('EA3C9A', '3514FF', nomaskDelta * 100),
+                  }}
+                >
+                  {word}
+                </ArchiveButton>
+              );
+            })}
+          </div>
         </div>
+        <div
+          className={classNames(
+            'transition-transform w-full h-full md:w-11/12 3xl:w-10/12 mx-auto overflow-y-scroll'
+          )}
+          id="comments-container"
+          ref={scroller}
+        >
+          <div className="grid grid-cols-2 gap-8 justify-around ">
+            {['promask', 'nomask'].map(origin => (
+              <GroupCommentList
+                key={origin}
+                origin={origin}
+                comments={comments}
+                chosen={chosen}
+                secondWord={secondWord}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
--- src/components/maskometerGrid.js
+++ src/components/maskometerGrid.js
@@ -1,94 +1,91 @@
-import React from "react";
-import classNames from "classnames";
-import {ArchiveButton} from "./Button";
-import {mix} from "../utils/mix";
-
+import React from 'react';
+import classNames from 'classnames';
+import {ArchiveButton} from './Button';
+import {mix} from '../utils/mix';
 
 export function MaskometerGrid({chosen, words, distribution, onClickSecondWord}) {
+  return (
+    <div className="h-full flex flex-col justify-between relative gap-4  ">
+      <p className="mx-8 mb-4 text-lg">
+        Horizontal position corresponds to usage by the two groups in the selcted corpus of reasons
+        to sign
+      </p>
+      <div className="flex-1 ">
+        <div
+          className={classNames(
+            ' h-full px-8 py-4 flex flex-col justify-between relative',
+            'overflow-hidden'
+          )}
+        >
+          <div className="z-1 uppercase">
+            <p
+              className="bg-promask text-white text-lg text-center h-full absolute top-0 bottom-0 left-0 rotate-180 p-1 z-10"
+              style={{writingMode: 'vertical-lr'}}
+            >
+              Promask
+            </p>
 
+            <p
+              className="bg-nomask text-white text-lg text-center h-full absolute top-0 bottom-0 right-0 p-1 z-10"
+              style={{writingMode: 'vertical-rl'}}
+            >
+              Nomask
+            </p>
 
-    return <div className="h-full flex flex-col justify-between relative gap-4  ">
-        <p className={"mx-8 mb-4 text-lg"}>
-            Horizontal position corresponds to usage by the two groups in the selcted corpus of reasons to sign
-        </p>
-        <div className="flex-1 ">
-            <div
-                className={classNames(" h-full px-8 py-4 flex flex-col justify-between relative", 'overflow-hidden')}>
-                <div className={"z-1 uppercase"}>
+            <div className="absolute top-0 bottom-0 right-1/2 translate-x-1/2 border-r-[1px] boreder-r-white border-dashed opacity-50" />
 
-                    <p className="bg-promask text-white text-lg text-center h-full absolute top-0 bottom-0 left-0 rotate-180 p-1 z-10"
-                       style={{writingMode: 'vertical-lr'}}>
-                        Promask
-                    </p>
+            <p className="text-lg text-sm 2xl:text-base text-center text-gray select-none absolute top-0 left-0 right-0 w-full normal-case bg-black pb-2">
+              equally used
+            </p>
+          </div>
 
-                    <p className="bg-nomask text-white text-lg text-center h-full absolute top-0 bottom-0 right-0 p-1 z-10"
-                       style={{writingMode: 'vertical-rl'}}>
-                        Nomask
-                    </p>
+          {typeof chosen !== 'undefined' &&
+            chosen.current &&
+            words.nodes.map(({name, deltaNomask}) => {
+              const isCurrent = chosen.next ? chosen.next === name : chosen.current === name;
 
-                    <div
-                        className="absolute top-0 bottom-0 right-1/2 translate-x-1/2 border-r-[1px] boreder-r-white border-dashed opacity-50"/>
+              let delta = isCurrent
+                ? deltaNomask
+                : distribution.find(({word, secondWord}) => {
+                    if (chosen.next) {
+                      return chosen.next === word && secondWord === name;
+                    }
+                    return chosen.current && word === chosen.current && secondWord === name;
+                  }).nomaskDelta;
 
-                    <p className="text-lg text-sm 2xl:text-base text-center text-gray select-none absolute top-0 left-0 right-0 w-full normal-case bg-black pb-2"
-                    >
-                        equally used
-                    </p>
+              delta = Math.round(delta * 100);
 
+              delta = 3 + ((97 - 3) / 100) * delta;
+
+              return (
+                <div
+                  key={name}
+                  className={classNames(
+                    'w-full flex items-center relative',
+                    'before:absolute before:left-0 before:right-0 ',
+                    'before:border-b-[1px] before:block before:top-1/2 before:transition-all',
+                    'before:w-full transition-[margin,background-color] duration-500',
+                    isCurrent
+                      ? 'before:border-b-gray  opacity-30'
+                      : 'before:border-b-current  before:opacity-20'
+                  )}
+                >
+                  <ArchiveButton
+                    isCurrent={isCurrent}
+                    style={{
+                      marginLeft: `${delta}%`,
+                      '--gradient-mix': isCurrent ? undefined : mix('EA3C9A', '3514FF', delta),
+                    }}
+                    className="transition-[margin] duration-1000 rounded-[0px] -translate-x-1/2 absolute z-20"
+                    onClick={() => onClickSecondWord(name)}
+                  >
+                    {name}
+                  </ArchiveButton>
                 </div>
-
-                {(typeof chosen !== 'undefined' && chosen.current) &&
-                words.nodes.map(({name, deltaNomask}) => {
-
-                    let isCurrent = chosen.next ? chosen.next === name : chosen.current === name
-
-                    isCurrent && console.log(name, deltaNomask)
-
-                    let delta =
-                        isCurrent ?
-                            deltaNomask :
-                            distribution
-                                .find(({
-                                           word,
-                                           secondWord
-                                       }) => {
-
-                                    if (chosen.next) {
-                                        return chosen.next === word && secondWord === name
-                                    }
-                                    return (chosen.current && word === chosen.current) && secondWord === name
-                                })['nomaskDelta']
-
-                    delta = Math.round(delta * 100)
-
-                    delta = 3 + ((97- 3) / (100)) * (delta)
-
-
-                    return <div key={name}
-                                className={
-                                    classNames(
-                                        "w-full flex items-center relative",
-                                        "before:absolute before:left-0 before:right-0 ",
-                                        "before:border-b-[1px] before:block before:top-1/2 before:transition-all",
-                                        "before:w-full transition-[margin,background-color] duration-500",
-                                        isCurrent ? 'before:border-b-gray  opacity-30' : 'before:border-b-current  before:opacity-20'
-                                    )
-                                }>
-                        <ArchiveButton
-                            isCurrent={isCurrent}
-                            style={{
-                                marginLeft: delta + "%",
-                                '--gradient-mix': isCurrent ? undefined : mix('EA3C9A', '3514FF', delta)
-                            }}
-                            className={"transition-[margin] duration-1000 rounded-[0px] -translate-x-1/2 absolute z-20"}
-                            onClick={() => onClickSecondWord(name)}
-                        >
-                            {name}
-                        </ArchiveButton>
-                    </div>
-                })
-                }
-            </div>
-
+              );
+            })}
         </div>
+      </div>
     </div>
+  );
 }
